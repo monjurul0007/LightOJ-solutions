37a720b5253caa01d96c903f6817983b8c250be0
Monjurul Hoque <mch.shuvo.official@gmail.com>
2020-04-18 13:55:53 +0600

    Create LightOJ_1094.cpp

---
+++ LightOJ_1094.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fasterInOut ios::sync_with_stdio(false); cin.tie(0);
+#define pb push_back
+#define inf INT_MAX
+#define neginf LLONG_MIN
+#define wk cout << "working" << endl
+#define all(x) x.begin(),x.end()
+#define mem(arr,value) memset(arr, value, sizeof(arr))
+#define mod 100000007
+#define F first  
+#define S second   
+
+typedef long long int ll;     
+typedef pair<ll , int>  pii;
+typedef pair<int , pii > piii; 
+
+vector <pii> adj[30005];
+ll dis[30005],n;
+
+void dijkstra(int s){
+	for(int i=0; i<n; i++){
+		dis[i] = LLONG_MAX;
+	}
+	priority_queue <pii, vector<pii> , greater<pii> > pq;
+	pq.push({0,s});
+	dis[s] = 0;
+	while(!pq.empty()){
+		int u = pq.top().second;
+		pq.pop();
+		for(int i=0; i<adj[u].size(); i++){
+			int v = adj[u][i].second;
+			int w = adj[u][i].first;
+			if(dis[u] + w < dis[v]){
+				dis[v] = dis[u] + w;
+				pq.push({dis[v],v});
+			}
+		}
+	}
+}
+
+int main(){
+	int t;
+	int u,v,w;
+	scanf("%d",&t);
+	for(int T=1; T<=t; T++){
+		scanf("%lld",&n);
+		
+		for(int i=0; i<n-1; i++){
+			scanf("%d %d %d",&u,&v,&w);
+			adj[u].pb({w,v});
+			adj[v].pb({w,u});
+		}
+		
+		ll node = 0,mx = 0;
+		dijkstra(0);
+		for(int i=0; i<n; i++){
+			if(mx < dis[i]){
+				mx = dis[i];
+				node = i;
+			}
+		}
+		
+		dijkstra(node);
+		for(int i=0; i<n; i++){
+			if(mx < dis[i]){
+				mx = dis[i];
+			}
+		}
+		
+		printf("Case %d: %lld\n",T,mx);
+		
+		for(int i=0; i<n; i++){
+			adj[i].clear();
+		}
+	}
+	
+    return 0;
+}
